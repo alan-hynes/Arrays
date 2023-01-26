a4b91abdf4b58f61e8c9f0822905e5ccfca098e7
G00400498 <g00400498@atu.ie>
2023-01-26 12:43:54 +0000

    final code

--- src/main/java/ie/atu/week1/Arrays.java
+++ src/main/java/ie/atu/week1/Arrays.java
@@ -9,35 +9,22 @@ public class Arrays {
     {
         //declares an array of integers
         ArrayList<Integer> myList = new ArrayList<>();
-        //int[] anArray;
-
-
-        //allocates memory for 10 integers
-        //anArray = new int[10];
 
         //initialise first element
         myList.add(100);
-        //anArray[0] = 100;
+
         //initialise second element
         myList.add(200);
-        //anArray[1] = 200;
+
         //initialise other elements
         myList.add(300);
-        //anArray[2] = 300;
         myList.add(400);
-        //anArray[3] = 400;
         myList.add(500);
-        //anArray[4] = 500;
         myList.add(600);
-        //anArray[5] = 600;
         myList.add(700);
-        //anArray[6] = 700;
         myList.add(800);
-        //anArray[7] = 800;
         myList.add(900);
-        //anArray[8] = 900;
         myList.add(1000);
-        //anArray[9] = 1000;
 
         loop(myList);
 
