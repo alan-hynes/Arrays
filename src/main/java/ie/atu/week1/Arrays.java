bdaa09330478aca9e0292761a545c4b795f987e2
G00400498 <g00400498@atu.ie>
2023-01-26 11:52:54 +0000

    branch 1

--- src/main/java/ie/atu/week1/Arrays.java
+++ src/main/java/ie/atu/week1/Arrays.java
@@ -1,8 +1,41 @@
 package ie.atu.week1;
 
+import java.util.Scanner;
+
 public class Arrays {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //declares an array of integers
+        int[] anArray;
+
+        //allocates memory for 10 integers
+        anArray = new int[10];
+
+        //initialise first element
+        anArray[0] = 100;
+        //initialise second element
+        anArray[1] = 200;
+        //initialise other elements
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        System.out.println("Element at index 0: " + anArray[0]);
+        System.out.println("Element at index 1: " + anArray[1]);
+        System.out.println("Element at index 2: " + anArray[2]);
+        System.out.println("Element at index 3: " + anArray[3]);
+        System.out.println("Element at index 4: " + anArray[4]);
+        System.out.println("Element at index 5: " + anArray[5]);
+        System.out.println("Element at index 6: " + anArray[6]);
+        System.out.println("Element at index 7: " + anArray[7]);
+        System.out.println("Element at index 8: " + anArray[8]);
+        System.out.println("Element at index 9: " + anArray[9]);
 
 
     }
